40c03eef784576c0e74a5c17214e5aabdfddd22e
vaibhavmishra1 <mishra.4@iitj.ac.in>
2023-07-30 00:07:25 +0530

    added get request

--- server/prog_lang_app.py
+++ server/prog_lang_app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def view_form():
     return render_template('login.html')
  
+...
+in_memory_datastore = {
+   "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
+   "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
+   "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
+}
+
+@app.get('/programming_languages')
+def list_programming_languages():
+   return {"programming_languages":list(in_memory_datastore.values())}
+
 @app.route('/handle_post', methods=['POST'])
 def handle_post():
     if request.method == 'POST':
