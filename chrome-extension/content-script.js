6db75912c0c490357f57a583dfdcb5ca8f44adfc
Anshul Kulhari <anshul.kulhari@avataar.me>
2023-07-30 09:12:25 +0530

    changes

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,26 +1,71 @@
 console.log("DISSOLVE: content script loaded");
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("DISSOLVE: message received",
+function runQuerySelector(jsonData) {
+  for (let i = 0; i < jsonData.selectorString.length; i++) {
+    document.querySelector(jsonData.selectorString[i]).click();
+  }
+}
+
+function executeQuery(jsonData) {
+  console.log("DISSOLVE:executing query", jsonData.selectorString);
+
+  if (data.redirectUrl != "") {
+    window.location.replace(jsonData.redirectUrl);
+    setTimeout(runQuerySelector(jsonData), 3000);
+  } else {
+    runQuerySelector(jsonData);
+  }
+}
+
+async function postToServer(query, htmlPayLoad, tabUrl) {
+  console.log("DISSOLVE:tried posting");
+  const url = "https://13.232.247.37:8080/handle_post";
+
+  const data = {
+    query: query,
+    htmlPayLoad: htmlPayLoad,
+    tabUrl: tabUrl,
+  };
+
+  let jsonData;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), // Convert the data to a valid JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+
+    jsonData = await response.json();
+    console.log("DISSOLVE:Response:", jsonData);
+  } catch (error) {
+    console.error("DISSOLVE:Error:", error);
+  }
+  executeQuery(jsonData);
+}
+
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  console.log(
+    "DISSOLVE: message received",
     sender.tab
       ? "from a content script:" + sender.tab.url
       : "from the extension"
   );
-  console.log("DISSOLVE:Dissolve is ready!!!")
-  if (request.action == "fetch html") {
-    console.log("DISSOLVE:fetching html")
-  }
-  if(request.action == "execute query"){
-    console.log("DISSOLVE:executing query",request.selectorString)
-    if(request.redirectUrl != ""){
-      window.location.replace(request.redirectUrl);
-    }
-    for (let i = 0; i < request.selectorString.length; i++) {
-      document.querySelector(request.selectorString[i]).click()
-    }
-  }
+  let htmlPayLoad = document.documentElement.outerHTML;
+
+  await postToServer(request.query, htmlPayLoad, request.tabUrl);
+  console.log("DISSOLVE:Dissolve is ready!!!");
+
   sendResponse({
     farewell: "goodbye",
-    htmlPayLoad: document.documentElement.outerHTML,
   });
 });
--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -3,57 +3,6 @@ console.log("DISSOLVE: script loaded");
 const textInput = document.getElementById("textInput");
 const submitButton = document.getElementById("dissolve");
 
-function executeQuery(jsonData) {
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(
-      activeTab.id,
-      {
-        action: "execute query",
-        selectorString:
-          ['span.color-swatch-inner[style*="background-image:url(https://resource.logitech.com/w_32,h_32,c_fill,b_white,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/swatch/rose.jpg?v=1)"]'],
-        redirectUrl: "",
-      },
-      function (response) {
-        console.log("DISSOLVE:", response);
-      }
-    );
-  });
-}
-
-async function postToServer(query, htmlPayLoad, tabUrl) {
-  console.log("tried posting");
-  const url = "http://13.232.247.37:8080/handle_post";
-
-  const data = {
-    query: query,
-    htmlPayLoad: htmlPayLoad,
-    tabUrl: tabUrl,
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data), // Convert the data to a valid JSON string
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-
-    const jsonData = await response.json();
-    console.log("Response:", jsonData);
-
-    executeQuery(jsonData);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
 function logToConsole() {
   console.log("DISSOLVE:logToConsole");
   const inputValue = textInput.value;
@@ -63,10 +12,9 @@ function logToConsole() {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(
       activeTab.id,
-      { action: "fetch html" },
+      { query: inputValue, tabUrl: activeTab.url },
       function (response) {
-        console.log("DISSOLVE:inputValue,response: ", inputValue, response);
-        postToServer(inputValue, response.htmlPayLoad, activeTab.url);
+        console.log("DISSOLVE:inputValue,response: ");
       }
     );
   });
