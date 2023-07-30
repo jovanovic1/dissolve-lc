e5a1c37008d2bcf6f3f35827bb7089cfd287ae06
Anshul Kulhari <anshul.kulhari@avataar.me>
2023-07-30 08:04:24 +0530

    added execute query flow

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,13 +1,24 @@
-console.log("anshul");
+console.log("DISSOLVE: content script loaded");
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
+  console.log("DISSOLVE: message received",
     sender.tab
       ? "from a content script:" + sender.tab.url
       : "from the extension"
   );
-  console.log(request.greeting);
-  if (request.greeting == "hello") console.log(document.body);
+  console.log("DISSOLVE:Dissolve is ready!!!")
+  if (request.action == "fetch html") {
+    console.log("DISSOLVE:fetching html")
+  }
+  if(request.action == "execute query"){
+    console.log("DISSOLVE:executing query",request.selectorString)
+    if(request.redirectUrl != ""){
+      window.location.replace(request.redirectUrl);
+    }
+    for (let i = 0; i < request.selectorString.length; i++) {
+      document.querySelector(request.selectorString[i]).click()
+    }
+  }
   sendResponse({
     farewell: "goodbye",
     htmlPayLoad: document.documentElement.outerHTML,
--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,35 +1,75 @@
-const textInput = document.getElementById('textInput');
-const submitButton = document.getElementById('dissolve');
+console.log("DISSOLVE: script loaded");
 
+const textInput = document.getElementById("textInput");
+const submitButton = document.getElementById("dissolve");
 
+function executeQuery(jsonData) {
 
-function logToConsole(){
-    console.log("anshul")
-
-    const inputValue = textInput.value;
-    
-    // // window.console.log('Hello from the extension!');
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {greeting: "hello"} , function(response) {
-            console.log(inputValue,response)
-        });
-        // You can now access the content of the active tab using the "activeTab" object.
-        // For example, you can access the URL of the active tab using activeTab.url.
-  
-        // Do whatever you want with the content of the active tab here.
-        console.log('URL of the active tab:', activeTab.url);
-      });
-
-    //   (async () => {
-    //     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    //     const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-    //     // do something with response here, not outside the function
-    //     // console.log(response);
-    //   })();
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(
+      activeTab.id,
+      {
+        action: "execute query",
+        selectorString:
+          ['span.color-swatch-inner[style*="background-image:url(https://resource.logitech.com/w_32,h_32,c_fill,b_white,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/swatch/rose.jpg?v=1)"]'],
+        redirectUrl: "",
+      },
+      function (response) {
+        console.log("DISSOLVE:", response);
+      }
+    );
+  });
 }
 
+async function postToServer(query, htmlPayLoad, tabUrl) {
+  console.log("tried posting");
+  const url = "http://13.232.247.37:8080/handle_post";
 
-submitButton.addEventListener('click', logToConsole);
-// document.getElementById('dissolve').addEventListener('click', logToConsole);
+  const data = {
+    query: query,
+    htmlPayLoad: htmlPayLoad,
+    tabUrl: tabUrl,
+  };
 
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), // Convert the data to a valid JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+
+    const jsonData = await response.json();
+    console.log("Response:", jsonData);
+
+    executeQuery(jsonData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function logToConsole() {
+  console.log("DISSOLVE:logToConsole");
+  const inputValue = textInput.value;
+  textInput.value = "";
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(
+      activeTab.id,
+      { action: "fetch html" },
+      function (response) {
+        console.log("DISSOLVE:inputValue,response: ", inputValue, response);
+        postToServer(inputValue, response.htmlPayLoad, activeTab.url);
+      }
+    );
+  });
+}
+
+submitButton.addEventListener("click", logToConsole);
